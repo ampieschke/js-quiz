f6803fd9fed35e36a1208abb4cc3f010d7db41c9
Andrew Pieschke <ampieschke@gmail.com>
2020-12-22 02:12:12 -0500

    started adding questions and working on rotating them in

--- Assets/script.js
+++ Assets/script.js
@@ -2,13 +2,19 @@
 const begin = document.querySelector("#beginButton");
 const timerEl = document.querySelector("#timer");
 const questionEl = document.querySelector("#question")
-const answers = document.querySelector("#answers")
-
+const answersEl = document.querySelector("#answers")
+const next = document.querySelector("#nextButton");
+var shuffledQuestions, currentQuestionIndex
 
 begin.addEventListener("click", startQuiz);
+next.addEventListener("click", () => {
+    currentQuestionIndex++
+    getNextQuestion()
+})
+
 function startQuiz() {
     begin.classList.add("hiden");
-    answers.classList.remove("hiden");
+    answersEl.classList.remove("hiden");
     beginTimer();
     beginQuiz()
 }
@@ -33,23 +39,41 @@ function beginTimer() {
 }
 
 function beginQuiz() {
-    questionEl.innerHTML = "I'm a question!";
-    //   buttonsEl.setAttribute("style, display:show")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    questionIndex = 0
+    getQuestion()
 }
-// function getQuestion() {
-//     var questions = [a, b, c];
-//     var questionOrder = shuffle(questions);
-//     console.log(questionOrder);
 
-//}
+function getQuestion() {
+    readQuestion(shuffledQuestions[questionIndex])
+}
 
-// function setNextQuestion() {
+function readQuestion(question) {
+    questionEl.innerText = question.question
 
-// }
+}
 
-// function selectAnswer() {
+var questions = [
+    {
+        question: "What is JavaScript?",
+        answers: [
+            { text: "A Dynamic Coding Language", correct: true },
+            { text: "An Anchient Egyption Archive System", correct: false },
+            { text: "Starbucks Drink", correct: false },
+            { text: "Acting Term", correct: false },
+        ],
 
-// }
+        question: "Which of These is not a JavaScript Data Type?",
+        answers: [
+            { text: "Array", correct: false },
+            { text: "Boolean", correct: false },
+            { text: "Texas", correct: true },
+            { text: "Object", correct: false },
+        ]
+
+    }
+
+]
 
 
 
