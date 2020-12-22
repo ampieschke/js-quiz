c0afc6a332d18ef34a9737d7deed7525558e146c
Andrew Pieschke <ampieschke@gmail.com>
2020-12-22 02:38:25 -0500

    have answer buttons trying to populate

--- Assets/script.js
+++ Assets/script.js
@@ -3,15 +3,22 @@ const begin = document.querySelector("#beginButton");
 const timerEl = document.querySelector("#timer");
 const questionEl = document.querySelector("#question")
 const answersEl = document.querySelector("#answers")
+const buttonAEl = document.querySelector("#btnA")
+const buttonBEl = document.querySelector("#btnB")
+const buttonCEl = document.querySelector("#btnC")
+const buttonDEl = document.querySelector("#btnD")
 const next = document.querySelector("#nextButton");
 var shuffledQuestions, currentQuestionIndex
 
+//Event Listeners
 begin.addEventListener("click", startQuiz);
-next.addEventListener("click", () => {
-    currentQuestionIndex++
-    getNextQuestion()
-})
+// next.addEventListener("click", () => {
+//     currentQuestionIndex++
+//     getNextQuestion()
+// })
 
+
+//Start Quiz
 function startQuiz() {
     begin.classList.add("hiden");
     answersEl.classList.remove("hiden");
@@ -22,16 +29,13 @@ function startQuiz() {
 // Set Time
 function beginTimer() {
     var timeRemaining = 60
-
     var timerInterval = setInterval(function () {
         timeRemaining--;
         timerEl.innerHTML = timeRemaining;
-
         if (timeRemaining <= 0) {
             clearInterval(timerInterval);
             timer.innerHTML = '---';
         }
-
         if (timeRemaining <= 9) {
             timerEl.setAttribute("style", "color:red");
         }
@@ -39,7 +43,7 @@ function beginTimer() {
 }
 
 function beginQuiz() {
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    shuffledQuestions = questions.sort()
     questionIndex = 0
     getQuestion()
 }
@@ -50,25 +54,28 @@ function getQuestion() {
 
 function readQuestion(question) {
     questionEl.innerText = question.question
-
+    buttonAEl.innerText = question.answers.text1
+    buttonBEl.innerText = question.answers.text
+    buttonCEl.innerText = question.text3
+    buttonDEl.innerText = answers.text4
 }
 
 var questions = [
     {
         question: "What is JavaScript?",
         answers: [
-            { text: "A Dynamic Coding Language", correct: true },
-            { text: "An Anchient Egyption Archive System", correct: false },
-            { text: "Starbucks Drink", correct: false },
-            { text: "Acting Term", correct: false },
+            { text1: "A Dynamic Coding Language" },
+            { text2: "An Anchient Egyption Archive System" },
+            { text3: "Starbucks Drink" },
+            { text4: "Acting Term" },
         ],
 
         question: "Which of These is not a JavaScript Data Type?",
         answers: [
-            { text: "Array", correct: false },
-            { text: "Boolean", correct: false },
-            { text: "Texas", correct: true },
-            { text: "Object", correct: false },
+            { text1: "Array" },
+            { text2: "Boolean" },
+            { text3: "Texas" },
+            { text4: "Object" },
         ]
 
     }
