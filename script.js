fe25d8bca7a47840b80bbe6dffd832fc77caff2a
Andrew Pieschke <ampieschke@gmail.com>
2020-12-21 22:58:23 -0500

    roughed out bootstrap layout

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-localStorage.setItem("high score".highScore)
+
+
+timeRemaining = 60
+//function startQuiz()
+//    beginTimer();
+//  beginQuiz()
+
 
 // Set Time
-var total = 60
-var seconds = Math.floor((total - now))
+function beginTimer() {
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+    var timerInterval = setInterval(function () {
+        timer.textContent = timeRemaining + " seconds left!";
+        timeRemaining--;
+        document.getElementById('timer').innerHTML = timeRemaining;
 
-// Update the count down every 1 second
-var timer = setTimer(function () {
+        if (timeRemaining <= 0) {
+            clearInterval(timerInterval);
+            timer.textContent = '---';
+        }
+    }, 1000);
+}
 
+beginButton.addEventLisener("click", beginTimer);
 
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(timer);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
+//localStorage.setItem("high score".highScore)
