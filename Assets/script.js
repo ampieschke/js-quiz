f425880bee8742b3073b21b2313dd3362f67d1d2
Andrew Pieschke <ampieschke@gmail.com>
2020-12-23 23:36:35 -0500

    added footer and enabled incorrect answers to deduct time

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 
 var begin = document.querySelector("#beginButton");
+var restart = document.querySelector("#restartButton");
 var timerEl = document.querySelector("#timer");
 var quizEl = document.querySelector("#quiz");
 var questionEl = document.querySelector("#question");
@@ -13,46 +14,29 @@ var hofButton = document.querySelector("#hofB");
 var hofListEl = document.querySelector("#hofList");
 var next = document.querySelector("#nextButton");
 var progEl = document.querySelector("#progress");
-var initialsInput = document.querySelector("#user-Initials-input")
-var userInitialsSpan = document.querySelector("#user-initials")
-var userScoreSpan = document.querySelector("#user-score")
+var initialsInput = document.querySelector("#user-Initials-input");
+var userInitialsSpan = document.querySelector("#user-initials");
+var userScoreSpan = document.querySelector("#user-score");
+
 
 //Quiz Vars
 var shuffledQuestions
 var questionIndex
+var userAnswer
+var lastUser
+var correct
+
 
 //Timer Vars
 var timerInterval
-var timeRemaining = 60
+var timeRemaining
 
+
+//Hall of Fame Vars
 var hof = [];
-
 
 //Begin Button Listener
 begin.addEventListener("click", startQuiz);
-
-//Answer Button Listeners
-buttonAEl.addEventListener("click", () => {
-    questionIndex++
-    console.log(questionIndex);
-    progEl.innerText = "progress: " + questionIndex + "/5";
-    getQuestion()
-})
-buttonBEl.addEventListener("click", () => {
-    questionIndex++
-    progEl.innerText = "progress: " + questionIndex + "/5";
-    getQuestion()
-})
-buttonCEl.addEventListener("click", () => {
-    questionIndex++
-    progEl.innerText = "progress: " + questionIndex + "/5";
-    getQuestion()
-})
-buttonDEl.addEventListener("click", () => {
-    questionIndex++
-    progEl.innerText = "progress: " + questionIndex + "/5";
-    getQuestion()
-})
 
 
 //Start Quiz
@@ -60,14 +44,14 @@ function startQuiz() {
     begin.classList.add("hiden");
     answersEl.classList.remove("hiden");
     beginTimer();
-    beginQuiz()
+    beginQuiz();
     progEl.innerText = "progress: " + questionIndex + "/5";
 }
 
 
 // Timer Start
 function beginTimer() {
-
+    timeRemaining = 60;
     timerInterval = setInterval(function () {
         timeRemaining--;
         timerEl.innerHTML = timeRemaining;
@@ -82,10 +66,66 @@ function beginTimer() {
 }
 
 
+//Timer Decuct
+function deductTime() {
+    timeRemaining = timeRemaining - 5;
+}
+
+
 // Timer Stop
 function stopTimer() {
     clearInterval(timerInterval);
 }
+
+//Answer Button Listeners
+buttonAEl.addEventListener("click", () => {
+    userAnswer = buttonAEl.innerText;
+    if (userAnswer !== correct) {
+        deductTime();
+    };
+    console.log(questionIndex);
+    console.log(userAnswer);
+    console.log(correct);
+    questionIndex++;
+    progEl.innerText = "progress: " + questionIndex + "/5";
+    getQuestion()
+})
+buttonBEl.addEventListener("click", () => {
+    userAnswer = buttonBEl.innerText;
+    if (userAnswer !== correct) {
+        deductTime();
+    };
+    console.log(questionIndex);
+    console.log(userAnswer);
+    console.log(correct);
+    questionIndex++;
+    progEl.innerText = "progress: " + questionIndex + "/5";
+    getQuestion()
+})
+buttonCEl.addEventListener("click", () => {
+    userAnswer = buttonCEl.innerText;
+    if (userAnswer !== correct) {
+        deductTime();
+    };
+    console.log(questionIndex);
+    console.log(userAnswer);
+    console.log(correct);
+    questionIndex++
+    progEl.innerText = "progress: " + questionIndex + "/5";
+    getQuestion()
+})
+buttonDEl.addEventListener("click", () => {
+    userAnswer = buttonDEl.innerText;
+    if (userAnswer !== correct) {
+        deductTime();
+    };
+    console.log(questionIndex);
+    console.log(userAnswer);
+    console.log(correct);
+    questionIndex++;
+    progEl.innerText = "progress: " + questionIndex + "/5";
+    getQuestion()
+})
 
 
 //Sort,Index, and Begin Asking Questions
@@ -113,13 +153,13 @@ function readQuestion(question) {
     buttonBEl.innerText = question.answers[1]
     buttonCEl.innerText = question.answers[2]
     buttonDEl.innerText = question.answers[3]
+    correct = question.correctAnswer
 }
 
 
 //End Game ToDos
 function endGame() {
     stopTimer();
-
     questionEl.innerText = "Quiz Complete! Enter Your Initials to Enter the Hall of Fame!"
     answersEl.classList.add("hiden");
     timerEl.innerHTML = "YAY!";
@@ -138,23 +178,34 @@ hofButton.addEventListener("click", function (event) {
         initials: initialsInput.value.trim(),
         score: timeRemaining
     };
+
     console.log(user)
     hof.push(user)
     console.log(hof)
-
-
     localStorage.setItem("user", JSON.stringify(user))
-    //localStorage.setItem("score", JSON.stringify(timeRemaining));
     runHof();
 }
 );
 
+
 //Show HOF
 function runHof() {
     quizEl.classList.add("hiden");
-    var lastUser = JSON.parse(localStorage.getItem("user"));
-    userInitialsSpan.innerHTML = lastUser.initials;
-    userScoreSpan.innerHTML = lastUser.score;
+    lastUser = JSON.parse(localStorage.getItem("user"));
+    userScoreSpan.append(lastUser.score);
+    userInitialsSpan.append(lastUser.initials);
+}
+
+
+//Restart Method
+restart.addEventListener("click", restartQuiz);
+function restartQuiz() {
+    quizEl.classList.remove("hiden");
+    answersEl.classList.remove("hiden");
+    hofListEl.classList.add("hiden");
+    beginTimer();
+    beginQuiz();
+    progEl.innerText = "progress: " + questionIndex + "/5";
 }
 
 
@@ -168,6 +219,7 @@ var questions = [
             "Starbucks Drink",
             "An Acting Term",
         ],
+        correctAnswer: "A Dynamic Coding Language"
     },
     {
         question: "Which of These is not a JavaScript Data Type?",
@@ -176,33 +228,37 @@ var questions = [
             "Boolean",
             "Texas",
             "Object",
-        ]
+        ],
+        correctAnswer: "Texas"
     },
     {
-        question: "3",
+        question: "Where should the JavaScript tag go?",
         answers: [
-            "Array",
-            "Boolean",
-            "Texas",
-            "Object",
-        ]
+            "The body",
+            "The Header",
+            "Inside your .css file",
+            "Both the body or header is correct",
+        ],
+        correctAnswer: "Both the body or header is correct"
     },
     {
-        question: "4",
+        question: "What is the proper way to enclose an array?",
         answers: [
-            "Array",
-            "Boolean",
-            "Texas",
-            "Object",
-        ]
+            "[]",
+            "()",
+            "{ }",
+            "//",
+        ],
+        correctAnswer: "[]"
     },
     {
-        question: "5",
+        question: "What CAN'T JavaScript do?",
         answers: [
-            "Array",
-            "Boolean",
-            "Texas",
-            "Object",
-        ]
+            "This homework assignment",
+            "Add to the HTML",
+            "Crush my soul",
+            "It can do all these things",
+        ],
+        correctAnswer: "It can do all these things"
     }
 ]
